b5a3d7be32028bd8a418ed28eca901d58cd3c01f
Lapo Bardotti <56101330+Lapo-Bardotti@users.noreply.github.com>
2021-11-18 20:01:15 -0300

    Add files via upload

---
+++ API_REST_LOJA/src/test/java/br/com/alura/loja/ClienteTest.java
@@ -0,0 +1,63 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+import junit.framework.Assert;
+
+public class ClienteTest {
+	
+	private HttpServer server;
+	private Client client;
+	private WebTarget target;
+	
+	@Before
+	public void startaServidor() {
+		server = Servidor.inicializaServidor();
+		this.client = ClientBuilder.newClient();
+		 this.target = client.target("http://localhost:8080");
+	}
+	
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOEsperado() {
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+
+	}
+	
+	@Test
+	public void testaQueSuportaNovosCarrinhos() {
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314, "Microfone", 37, 1));
+		carrinho.setRua("Rua Vergueiro 3185");
+		carrinho.setCidade("Sao Paulo");
+		String xml = carrinho.toXML();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals(201, response.getStatus());
+		String location = response.getHeaderString("Location");
+		String conteudo = client.target(location).request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Microfone"));
+	}
+
+}
